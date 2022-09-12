778227cdcd0dcaa1cdeefa63f22aa1e6dc938d87
Itsrhoda” <rodahmanyaga.com>
2022-09-12 14:49:22 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
-#include "_putchar.h"
+#include <main.h>
 
 /**
- * main - Prints _putchar
+ * main - Prints _putchar.
  *
- * Return: Always 0 (Success)
+ * Returns: Always 0
  */
 int main(void)
 {
-	char _putchar[9] - {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	char _putchar[8] - {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
 	for (c - 0; c < sizeof(_putchar); c++)

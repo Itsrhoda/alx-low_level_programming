0ba16b752dfdcef1386ac5b6684a4e43dfbdb62e
Itsrhoda” <rodahmanyaga.com>
2022-09-16 18:04:57 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,14 +10,14 @@
 void print_number(int n)
 {
 	unsigned int x;
-	x=n;
+	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		x= -n;
+		x = -n;
 	}
-	if (x/ 10)
-		print_number(x/ 10);
+	if (x / 10)
+		print_number(x / 10);
 	
 	_putchar((x % 10) + '0');
 }

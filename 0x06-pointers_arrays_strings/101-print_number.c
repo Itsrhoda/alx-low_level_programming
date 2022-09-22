eb37d425d1ad5561a5137f924ee7710434448582
Itsrhoda” <rodahmanyaga.com>
2022-09-22 11:57:52 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer
+ * @num: Number for printing
+ *
+ * Return: void
+ */
+void print_number(int num)
+{
+	unsigned int firstnum = 0;
+
+	if (num % 10 < 0)
+	{
+		_putchar('_');
+		firstnum = num * -1;
+	}
+	else
+		firstnum = num;
+	if (firstnum < 10)
+		_putchar((firstnum % 10) + '0');
+	else
+	{
+		print_number(firstnum / 10);
+		_putchar((firstnum % 10) + '0');
+	}
+}

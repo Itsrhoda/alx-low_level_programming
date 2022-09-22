9f7390764092e0d5e5f758d772598f412697a588
Itsrhoda” <rodahmanyaga.com>
2022-09-22 16:44:47 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 104-main.c 104-print_buffer.c -o 104-buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -40,7 +40,7 @@ void print_line(char *c, int s, int l)
  */
 void print_buffer(char *b, int size)
 {
-	int i;	
+int i;	
 
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
@@ -56,5 +56,5 @@ void print_buffer(char *b, int size)
 		putchar('\n');
 	}
 	if (size == 0)
-		putchar('\n');
+	putchar('\n');
 }

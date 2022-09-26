5695d0b2e954a1f3e61592f34b380f0a26a4959f
Itsrhoda” <rodahmanyaga.com>
2022-09-26 04:23:38 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-memset.c
+ * Auth: Rhoda Manyaga
+ */
+
+#include "main.h"
+
+/**
+ * memset - Fills the first n bytes of the memory area
+ * 	    pointed to by @s with the constant byte @c.
+ * @c: The character to fill the memory area with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: Apointer to be filled memory area @s.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int index;
+	unsigned char *memory = s, value = c;
+
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+
+	return (memory);
+}

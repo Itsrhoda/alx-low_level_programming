52e4368c6914ef03d19293fd0ec014b9cf447262
Itsrhoda” <rodahmanyaga.com>
2022-09-26 11:43:03 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,9 +25,8 @@ char *_strstr(char *haystack, char *needle)
 			do {
 				if (needle[index + 1] == '\0')
 					return (haystack);
-				index++;
-
-		        } while (haystack[index] == needle[index]);
+				index++;	       
+			} while (haystack[index] == needle[index]);
 		}
 		haystack++;
 	}

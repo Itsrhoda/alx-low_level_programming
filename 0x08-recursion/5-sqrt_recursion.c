f736d32a76452408df4dd8dd17cd628127a361bd
Itsrhoda” <rodahmanyaga.com>
2022-09-28 10:52:40 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,4 +13,16 @@ int helper(int a, int b)
 	else
 		return (helper(a, b + 1));
 	return (1);
+
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to return the square root of
+ * Return: the square root of @n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (helper(n, 1));
 }

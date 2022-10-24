d8e01e895a460dde286775094b460d02c555ea11
Itsrhoda” <rodahmanyaga.com>
2022-10-24 06:22:52 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,22 @@
 #include "lists.h"
+
 /**
- * add_note - adds a new mode at the beginning
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * add_node - adds a new mode at the beginning
  * of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.

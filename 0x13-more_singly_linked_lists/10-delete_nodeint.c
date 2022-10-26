ea2b58a5c96190ab2f29e5758cd57224439eca67
Itsrhoda” <rodahmanyaga.com>
2022-10-26 14:51:02 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,6 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *prev;
 	listint_t *next;
 
+	prev = *head;
+
 	if (index != 0)
 	{
 		for (i = 0; i < index - 1 && prev != NULL; i++)

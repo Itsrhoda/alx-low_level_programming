ac5bff63209b9162ef02a660835d947c152283c1
Itsrhoda” <rodahmanyaga.com>
2022-11-02 17:16:57 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,14 +1,11 @@
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
  * create_file - Create a file with a specified name and content
  * @filename: Name of the file to create
  * @text_content: Content to read into the file
  *
- * Return: 1 on success, -1 on any failure
+ * Return: 1
  */
 int create_file(const char *filename, char *text_content)
 {

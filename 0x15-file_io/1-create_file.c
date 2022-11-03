4bfd8bbf44a0dfcc1f11959e2b96ca87b14e76cf
Itsrhoda” <rodahmanyaga.com>
2022-11-02 17:19:20 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - Create a file with a specified name and content
  * @filename: Name of the file to create
- * @text_content: Content to read into the file
+ * @text_content: null terminated string to write
  *
  * Return: 1
  */
@@ -13,6 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	file = open(filename, O_CREATY | o_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 		return (-1);

1cd130a24e6fc01e3797d0acab880cc6875d2648
Itsrhoda” <rodahmanyaga.com>
2022-11-03 18:16:17 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,12 +3,12 @@
 /**
  * append_text_to_file - appends text at the end of a fil
  * @filename: filename.
- * text_content: added content.
+ * @text_content: added content.
  *
  * Return: 1 if the file exists. -1 if the fails does not exist
  * or if it fails.
  */
-int append_text_to_file(const char *filename char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;

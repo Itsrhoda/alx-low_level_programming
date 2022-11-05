e0e8d157243de945e5eaf98bed2ebe91c7e731d5
Itsrhoda” <rodahmanyaga.com>
2022-11-05 02:28:09 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREATY | O_WRONLY | o_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(fd, textcontent, nletters);
+	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);

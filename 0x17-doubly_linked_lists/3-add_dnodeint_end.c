b67311c4336ed7656e6f15b8d8272841ade62529
Itsrhoda” <rodahmanyaga.com>
2022-12-08 13:42:03 -0800

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *h;
 	dlistint_t *new;
 
-	new = maloc(sizeof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 

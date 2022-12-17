2539a2d3b2d3112267dcc8bbfc7228672ecea066
Itsrhoda” <rodahmanyaga.com>
2022-12-17 12:29:39 -0800

    testfile

---
+++ 0x18-dynamic_libraries/testfile.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - to test if library worked
+ *
+ * Return: 0 if successful else 1
+ */
+int main(void)
+{
+	printf("==> %d\n", _strlen("Itsrhoda"));
+
+	return (0);
+}

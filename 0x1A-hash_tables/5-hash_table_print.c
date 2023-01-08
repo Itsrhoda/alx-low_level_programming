4d4b3599c2ebe3698dce5433576c888f51000965
Itsrhoda” <rodahmanyaga.com>
2023-01-08 11:05:17 -0800

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@
  * Description: Key/value pairs are printed in the order
  * they appear in the array of the hash table.
  */
-void hash_table_print(const hash_table_t * ht)
+void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
 	unsigned long int i;
